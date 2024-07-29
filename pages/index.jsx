13656b4376836800c99edd83a3ad62e865fb26ec
ronniechoyy <61714797+ronniechoyy@users.noreply.github.com>
2024-07-29 14:15:26 +0800

    Add tailwind, nextui

---
+++ pages/index.jsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { Button } from "@nextui-org/react";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <div className=" absolute inset-0 grid place-content-center">
+      <div>
+        MAIN PAGE
+      </div>
+      <Button>
+        Click me
+      </Button>
+    </div>
+
+  );
+}
