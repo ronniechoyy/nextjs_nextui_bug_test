5f43c59cac255f02861cf2ee486ce43551d19e86
ronniechoyy <61714797+ronniechoyy@users.noreply.github.com>
2024-07-29 14:22:06 +0800

    add about page

---
+++ pages/About/index.jsx
@@ -0,0 +1,15 @@
+import { Button } from "@nextui-org/react";
+
+export default function About() {
+  return (
+    <div className=" absolute inset-0 grid place-content-center">
+      <div>
+        About
+      </div>
+      <Button>
+        Click me
+      </Button>
+    </div>
+
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -6,12 +6,14 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className=" absolute inset-0 grid place-content-center">
+    <div className=" absolute inset-0 grid place-content-center text-center">
       <div>
         MAIN PAGE
       </div>
-      <Button>
-        Click me
+      <Button onClick={()=>{
+        window.location.href = "/About";
+      }}>
+        Click me to about
       </Button>
     </div>
 
