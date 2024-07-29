6e251113a8a7b0c4f8729906f3f1c255917f56b4
ronniechoyy <61714797+ronniechoyy@users.noreply.github.com>
2024-07-29 15:26:38 +0800

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { Button as MUIButton } from "@mui/material";
-import { Button } from "@nextui-org/react";
+import { Button, NextUIProvider } from "@nextui-org/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
+    <NextUIProvider>
     <div className=" absolute inset-0 grid place-content-center text-center">
       <div>
         MAIN PAGE
@@ -24,6 +25,7 @@ export default function Home() {
       </MUIButton>
       
     </div>
+    </NextUIProvider>
 
   );
 }
